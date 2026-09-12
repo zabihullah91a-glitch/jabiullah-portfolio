cfdbc4588a21e731638a5be225c579324ac3cd5e
Jabiullah <zabihullah91a@gmail.com>
2026-09-12 15:59:10 +0530

    Prepare app for deployment

--- app.js
+++ app.js
@@ -112,10 +112,8 @@ app.use((req, res) => {
 // SERVER
 // ========================================
 
-const PORT = 4000;
-
-app.listen(PORT, () => {
-
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, "0.0.0.0", () => {
     console.log(
         `PORTFOLIO SERVER: http://localhost:${PORT}`
     );
