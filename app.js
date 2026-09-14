3862e5a9a193af09a8d2bc34e3f83f245c4c31d6
Jabiullah <zabihullah91a@gmail.com>
2026-09-14 16:47:39 +0530

    Add sitemap

--- app.js
+++ app.js
@@ -27,7 +27,19 @@ app.use(
 
 app.set("view engine", "ejs");
 app.set("views", viewsPath);
+// SITEMAP
+app.get("/sitemap.xml", (req, res) => {
+    res.type("application/xml");
 
+    res.send(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+    <url>
+        <loc>https://jabiullah-portfolio.onrender.com/</loc>
+    </url>
+
+</urlset>`);
+});
 
 // HOME PAGE
 app.get("/", (req, res) => {
